66a7e13e7b686ad873f2df6cb8f186bb3fdba002
SvitlanaSavchenko <134533907+SvitlanaSavchenko@users.noreply.github.com>
2024-12-01 15:43:32 +0100

    hw done

---
+++ task_01/config.py
@@ -0,0 +1,12 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'postgres',
+    'user': 'postgres',
+    'password': 'thetrisss'
+}
+
+
+NUMBER_USERS = 10
+NUMBER_TASKS = 20
+STATUSES = ["new", "in progress", "completed"]
